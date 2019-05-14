43cf65e54ed8e5a167990e464f13bbeebb702502
Caroline Lin <ruoyi.lin@gmail.com>
2019-05-14 17:58:30 -0400

    add 'highlight' a word' function

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,34 @@ import * as React from "react";
 import { useState } from "react";
 import ReactDOM from "react-dom";
 
+/**
+Input:
+    hello world bob yay hello
+    hello
+Output:
+    *hello* world bob yay *hello*
+**/
+function FormatText(text: string, textitem: string): string {
+  const words = text.split(" ");
+  const output = words.map(word => {
+    if (word === textitem) {
+      return `*${word}*`;
+    } else {
+      return `${word}`;
+    }
+  });
+  return output.join(" ");
+}
+
 function App(props) {
   let s: string = "foo";
   const [text, setText] = useState(s);
 
-  return (
-    [
-      <textarea onChange={e => setText(e.target.value)} value={text} />,
-      <div>{text}</div>,
-      
-    ]
-  );
+  return [
+    <textarea onChange={e => setText(e.target.value)} value={text} />,
+    <div>{text}</div>,
+    <div>{FormatText(text, "hello")}</div>
+  ];
 }
 const appdiv = document.getElementById("app");
 
