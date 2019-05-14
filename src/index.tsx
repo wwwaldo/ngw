d09f955059f36124d6886140b38174c649181770
Caroline Lin <ruoyi.lin@gmail.com>
2019-05-14 17:49:45 -0400

    textboxes in flexbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom";
 
 function App(props) {
-    return <h1>Hello world</h1>;
-}
-const appdiv = document.getElementById('app');
+  let s: string = "foo";
+  const [text, setText] = useState(s);
 
-ReactDOM.render(<App/>, appdiv);
+  return (
+    [
+      <textarea onChange={e => setText(e.target.value)} value={text} />,
+      <div>{text}</div>,
+      
+    ]
+  );
+}
+const appdiv = document.getElementById("app");
+
+ReactDOM.render(<App />, appdiv);
